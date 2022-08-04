4f578498694466089494512207c2ff59f174f5cd
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-04 20:24:46 +0200

    Add title and description in WelcomePage

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:adwaita/adwaita.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'main_page.dart';
 import 'widgets/rounded_app/rounded_app_widget.dart';
@@ -14,6 +15,8 @@ class FlashApp extends StatelessWidget {
         title: "Flash",
         theme: AdwaitaThemeData.light(),
         darkTheme: AdwaitaThemeData.dark(),
+        localizationsDelegates: const [AppLocalizations.delegate],
+        supportedLocales: const [Locale("en")],
         debugShowCheckedModeBanner: false,
         themeMode: ThemeMode.system,
         home: _getHome(),
--- lib/pages/welcome_page/welcome_page.dart
+++ lib/pages/welcome_page/welcome_page.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
+import 'package:flash/l10n/l10n.dart';
 import 'package:flash/pages/welcome_page/widgets/welcome_icon_widget.dart';
 import 'package:flash/widgets/transparent_divider/transparent_divider_widget.dart';
+import 'package:flutter/material.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -13,10 +14,39 @@ class WelcomePage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          const WelcomeIconWidget(),
-          TransparentDividerWidget(height: 25)
+          ..._getWelcomeIconWithBottomDivider(context),
+          ..._getAppTitleWithBottomDivider(context),
+          ..._getAppDescription(context)
         ],
       ),
     );
+  }
+
+  List<Widget> _getWelcomeIconWithBottomDivider(BuildContext context) {
+    return const [
+      WelcomeIconWidget(),
+      TransparentDividerWidget(height: 25),
+    ];
+  }
+
+  List<Widget> _getAppTitleWithBottomDivider(BuildContext context) {
+    return [
+      Text(
+        context.l10n.appTitle,
+        style: Theme.of(context).textTheme.headline1,
+      ),
+      const TransparentDividerWidget(height: 15),
+    ];
+  }
+
+  List<Widget> _getAppDescription(BuildContext context) {
+    return [
+      SizedBox(
+          width: 300,
+          child: Text(
+            context.l10n.appDescription,
+            textAlign: TextAlign.center,
+          ))
+    ];
   }
 }
