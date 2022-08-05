7b92829ea388129792a4e852d409da1ace7bb564
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-05 23:36:15 +0200

    Implement tests for DependencyChecker

---
+++ test/utils/dependency_checker_test/dependency_checker_test.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:flash/utils/command_executor/command_executor.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'dependency_checker_test.mocks.dart';
+
+@GenerateMocks([CommandExecutor])
+void main() {
+  group("DependencyChecker", () {
+    late MockCommandExecutor mockCommandExecutor;
+    late DependencyChecker dependencyChecker;
+    setUp(() => {
+      mockCommandExecutor =MockCommandExecutor();
+        dependencyChecker =
+        DependencyChecker(commandExecutor: mockCommandExecutor);
+    });
+
+    test("should return false if the dependency not exists", () {
+      configureToReturnValueThatSaysItNotExists(mockCommandExecutor);
+
+      expect(dependencyChecker.checkIfExists("application"), false);
+    });
+
+    test("should return true if dependency exists", () {
+      configureToReturnValueThatSaysItExists(mockCommandExecutor);
+
+      expect(dependencyChecker.checkIfExists("application"), true);
+    });
+  });
+}
+
+void configureToReturnValueThatSaysItNotExists(
+    MockCommandExecutor mockCommandExecutor) {
+  when(mockCommandExecutor.execute)
+      .thenReturn((executable, arguments) => "$executable: ");
+}
+
+void configureToReturnValueThatSaysItExists(
+    MockCommandExecutor mockCommandExecutor) {
+  when(mockCommandExecutor.execute).thenReturn((executable, arguments) =>
+      "$executable: /bin/$executable, /usr/bin/$executable");
+}
