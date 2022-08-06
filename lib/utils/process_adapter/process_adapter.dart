6da047dfbb7414bf16208636484cbb6595707483
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-06 23:35:50 +0200

    Add comment to ProcessAdapter

--- lib/utils/process_adapter/process_adapter.dart
+++ lib/utils/process_adapter/process_adapter.dart
@@ -1,5 +1,9 @@
 import 'dart:io';
 
+/*
+  This ProcessAdapter was implemented to allow unit testing.
+  Mockito can't mock static methods.
+ */
 class ProcessAdapter {
   String execute(String command, List<String> arguments) {
     var output = Process.runSync(command, arguments);
