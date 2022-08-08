4f49e876480154c2a4b0e5add299ef8afc6cc576
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-09 00:47:03 +0200

    Add Title in GamesPage

--- lib/pages/games/games_page.dart
+++ lib/pages/games/games_page.dart
@@ -1,3 +1,5 @@
+import 'package:flash/l10n/l10n.dart';
+import 'package:flash/widgets/transparent_divider/transparent_divider_widget.dart';
 import 'package:flutter/material.dart';
 
 class GamesPage extends StatelessWidget {
@@ -5,6 +7,19 @@ class GamesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [_buildTitle(context)],
+    );
+  }
+
+  Widget _buildTitle(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Column(children: [
+        Text(context.l10n.games, style: Theme.of(context).textTheme.headline1),
+        const TransparentDividerWidget(height: 10),
+        Text(context.l10n.gamesPageDescription)
+      ]),
+    );
   }
 }
