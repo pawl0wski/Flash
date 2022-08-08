d0880e95a89d01e4d1495fc9a3c70bddfed7573b
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-08 23:41:12 +0200

    Add Games in SidebarWidget

--- lib/widgets/sidebar/sidebar_widget.dart
+++ lib/widgets/sidebar/sidebar_widget.dart
@@ -1,3 +1,5 @@
+import 'package:adwaita_icons/adwaita_icons.dart';
+import 'package:flash/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:libadwaita/libadwaita.dart';
 
@@ -15,10 +17,29 @@ class SidebarWidget extends StatelessWidget {
         currentIndex: currentIndex,
         isDrawer: isDrawer,
         onSelected: (index) => {},
-        children: _getSidebarItems());
+        children: _getSidebarItems(context));
   }
 
-  List<AdwSidebarItem> _getSidebarItems() {
-    return [];
+  List<AdwSidebarItem> _getSidebarItems(BuildContext context) {
+    return [
+      _createSidebarItem(label: context.l10n.games, icon: AdwaitaIcons.gamepad)
+    ];
+  }
+
+  AdwSidebarItem _createSidebarItem(
+      {required String label, required String icon}) {
+    return AdwSidebarItem(
+        labelWidget: Row(
+      children: [
+        AdwaitaIcon(
+          icon,
+          size: 20,
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        Text(label)
+      ],
+    ));
   }
 }
