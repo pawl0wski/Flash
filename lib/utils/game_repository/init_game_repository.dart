15f72767dbd2ffe271d38a5ae3693d7e413cd36f
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-09 21:40:16 +0200

    Open games box in initGameRepository

--- lib/utils/game_repository/init_game_repository.dart
+++ lib/utils/game_repository/init_game_repository.dart
@@ -3,6 +3,8 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 Future<void> initGameRepository() async {
   _registerAdapterIfNotRegistered(GameAdapter());
+
+  await Hive.openBox<Game>("games");
 }
 
 void _registerAdapterIfNotRegistered(TypeAdapter adapter) {
