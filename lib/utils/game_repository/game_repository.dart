67642668abeba48edbce64b35ede2ee53dab4631
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-10 23:47:35 +0200

    Make _gameHiveBox final in GameRepository

--- lib/utils/game_repository/game_repository.dart
+++ lib/utils/game_repository/game_repository.dart
@@ -5,7 +5,7 @@ import 'package:uuid/uuid.dart';
 import 'hive_box/game_hive_box.dart';
 
 class GameRepository {
-  GameHiveBox _gameHiveBox;
+  final GameHiveBox _gameHiveBox;
 
   GameRepository({GameHiveBox? gameHiveBox})
       : _gameHiveBox = gameHiveBox ?? GameHiveBox();
