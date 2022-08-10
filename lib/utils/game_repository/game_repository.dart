77572ad83e447031037a8c84c4307139eada7620
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-10 23:42:03 +0200

    Create getAllGames method in GameRespository

--- lib/utils/game_repository/game_repository.dart
+++ lib/utils/game_repository/game_repository.dart
@@ -1,3 +1,6 @@
+import 'package:flash/utils/game_repository/models/game.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
 import 'hive_box/game_hive_box.dart';
 
 class GameRepository {
@@ -5,4 +8,15 @@ class GameRepository {
 
   GameRepository({GameHiveBox? gameHiveBox})
       : _gameHiveBox = gameHiveBox ?? GameHiveBox();
+
+  List<Game> getAllGames() {
+    List<Game> games = [];
+    for (var gameUuid in _box.keys) {
+      var game = _box.get(gameUuid);
+      if (game != null) games.add(game);
+    }
+    return games;
+  }
+
+  Box<Game> get _box => _gameHiveBox.box;
 }
