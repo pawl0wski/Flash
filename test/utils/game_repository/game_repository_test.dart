67ad6ef97d3f4e908e8643ec4314f7c098d21855
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-11 00:21:22 +0200

    Mock GameHiveBox and Uuid in game_repository_test

--- test/utils/game_repository/game_repository_test.dart
+++ test/utils/game_repository/game_repository_test.dart
@@ -1,5 +1,19 @@
+import 'package:flash/utils/game_repository/hive_box/game_hive_box.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:uuid/uuid.dart';
 
+import 'game_repository_test.mocks.dart';
+
+@GenerateMocks([Uuid, GameHiveBox])
 void main() {
-  group("GameRepository", () {});
+  group("GameRepository", () {
+    late MockGameHiveBox mockGameHiveBox;
+    late MockUuid mockUuid;
+
+    setUp(() {
+      mockGameHiveBox = MockGameHiveBox();
+      mockUuid = MockUuid();
+    });
+  });
 }
