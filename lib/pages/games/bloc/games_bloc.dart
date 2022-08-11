5cbfa550d9147a91ca5736787e02487148904fe3
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-12 00:09:12 +0200

    Add GameRepository to GamesBloc

--- lib/pages/games/bloc/games_bloc.dart
+++ lib/pages/games/bloc/games_bloc.dart
@@ -1,11 +1,16 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flash/utils/game_repository/game_repository.dart';
 
 part 'games_event.dart';
 part 'games_state.dart';
 
 class GamesBloc extends Bloc<GamesEvent, GamesState> {
-  GamesBloc() : super(const GamesState(games: [])) {
+  final GameRepository _gameRepository;
+
+  GamesBloc({GameRepository? gameRepository})
+      : _gameRepository = gameRepository ?? GameRepository(),
+        super(const GamesState(games: [])) {
     on<GamesEvent>((event, emit) {
       // TODO: implement event handler
     });
