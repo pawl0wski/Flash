6427d1020161054ff309561e1cae1a416ab4f78f
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-13 00:11:50 +0200

    Provide the listener and builder in the constructor in GamesPage

--- lib/pages/games/games_page.dart
+++ lib/pages/games/games_page.dart
@@ -1,4 +1,3 @@
-import 'package:flash/dialog/add_game_dialog/add_game_dialog.dart';
 import 'package:flash/pages/games/utils/games_page_builder/games_page_builder.dart';
 import 'package:flash/pages/games/utils/games_page_listener/games_page_listener.dart';
 import 'package:flutter/material.dart';
@@ -7,11 +6,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'bloc/games_bloc.dart';
 
 class GamesPage extends StatelessWidget {
-  const GamesPage({Key? key}) : super(key: key);
+  final GamesPageBuilder _builder;
+  final GamesPageListener _listener;
 
-  GamesPageBuilder get _builder => const GamesPageBuilder();
-
-  GamesPageListener get _listener => const GamesPageListener();
+  const GamesPage(
+      {GamesPageBuilder? builder, GamesPageListener? listener, Key? key})
+      : _listener = listener ?? const GamesPageListener(),
+        _builder = builder ?? const GamesPageBuilder(),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
