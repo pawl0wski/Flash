975bd09b56f609f781fff884aa942b729242b5d2
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-13 23:23:32 +0200

    Provide XPropValidator in XPropCommand

--- lib/utils/commands/xprop_command.dart
+++ lib/utils/commands/xprop_command.dart
@@ -1,9 +1,12 @@
 import 'package:flash/utils/commands/command.dart';
 import 'package:flash/utils/commands/scrapers/xprop_scraper.dart';
+import 'package:flash/utils/commands/validators/xprop_validator.dart';
 import 'package:flash/utils/process_adapter/process_adapter.dart';
 
 class XPropCommand extends Command {
   XPropCommand({ProcessAdapter? processAdapter})
       : super("xprop",
-            processAdapter: processAdapter, commandScraper: XPropScraper());
+            processAdapter: processAdapter,
+            commandScraper: XPropScraper(),
+            validator: XPropValidator());
 }
