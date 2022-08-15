a93849b4244339a1fdf5db75714dbc775678027c
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-15 22:23:56 +0200

    Add XDoToolCommand in AddGameBloc

--- lib/dialog/add_game_dialog/bloc/add_game_bloc.dart
+++ lib/dialog/add_game_dialog/bloc/add_game_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flash/utils/commands/xdotool_command.dart';
 import 'package:flash/utils/commands/xprop_command.dart';
 import 'package:meta/meta.dart';
 
@@ -8,9 +9,11 @@ part 'add_game_state.dart';
 
 class AddGameBloc extends Bloc<AddGameEvent, AddGameState> {
   final XPropCommand _xPropCommand;
+  final XDoToolCommand _xDoToolCommand;
 
-  AddGameBloc({XPropCommand? xPropCommand})
+  AddGameBloc({XPropCommand? xPropCommand, XDoToolCommand? xDoToolCommand})
       : _xPropCommand = xPropCommand ?? XPropCommand(),
+        _xDoToolCommand = xDoToolCommand ?? XDoToolCommand(),
         super(AddGameStateClickOnGame()) {
     on<AddGameEventShowClickOnGame>(_onShowClickOnGame);
   }
