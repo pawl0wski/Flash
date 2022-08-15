fa6bdca78ab33535c81a2cb30b59be5033b8f613
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-15 21:44:32 +0200

    Implement _getWindowNameByWindow in xdotool_command

--- lib/utils/commands/xdotool_command.dart
+++ lib/utils/commands/xdotool_command.dart
@@ -1,13 +1,26 @@
+import '../game_repository/models/game.dart';
 import 'command.dart';
 import 'exceptions/cant_find_window.dart';
 
 class XDoToolCommand extends Command {
   XDoToolCommand() : super("xdotool");
 
+  Game getGameFromPid(int pid) {
+    var game = Game();
+    var window = _getFirstWindowByPid(pid);
+
+    return game;
+  }
+
   int _getFirstWindowByPid(int pid) {
     List<String> windows =
         execute(["search", "--pid", pid.toString()]).split("\n");
     if (windows.isEmpty) throw CantFindWindow();
     return int.parse(windows.first);
+  }
+
+  String _getWindowNameByWindow(int window) {
+    var windowName = execute(["getwindowname", window.toString()]);
+    return windowName;
   }
 }
