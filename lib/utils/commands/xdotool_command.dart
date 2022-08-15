3e8d1116dd95526106a22f72c6ce7bbb04e7c406
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-15 21:40:16 +0200

    Implement _getFirstWindowByPid in xdotool_command

--- lib/utils/commands/xdotool_command.dart
+++ lib/utils/commands/xdotool_command.dart
@@ -1,5 +1,13 @@
 import 'command.dart';
+import 'exceptions/cant_find_window.dart';
 
 class XDoToolCommand extends Command {
   XDoToolCommand() : super("xdotool");
+
+  int _getFirstWindowByPid(int pid) {
+    List<String> windows =
+        execute(["search", "--pid", pid.toString()]).split("\n");
+    if (windows.isEmpty) throw CantFindWindow();
+    return int.parse(windows.first);
+  }
 }
