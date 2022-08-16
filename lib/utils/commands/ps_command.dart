7f169d6d3a9e1b6bf68cf09eaf0929e99475f23b
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-16 02:15:11 +0200

    Add PsCommand

---
+++ lib/utils/commands/ps_command.dart
@@ -0,0 +1,18 @@
+import 'package:flash/utils/commands/command.dart';
+import 'package:flash/utils/commands/scrapers/models/scraped_ps.dart';
+import 'package:flash/utils/commands/scrapers/ps_scraper.dart';
+
+class PsCommand extends Command {
+  PsCommand()
+      : super("ps", defaultArguments: ["aux"], commandScraper: PsScrapper());
+
+  @override
+  ScrapedPs executeAndScrap(List<String> arguments) {
+    return super.executeAndScrap(arguments) as ScrapedPs;
+  }
+
+  @override
+  Future<ScrapedPs> executeAndScrapAsync(List<String> arguments) async {
+    return super.executeAndScrapAsync(arguments) as ScrapedPs;
+  }
+}
