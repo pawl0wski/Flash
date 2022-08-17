87ab4f34830ff4133614483eaf089db16a28e95f
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-17 21:40:53 +0200

    Add deleteGame in l10n

--- lib/pages/games/widgets/games_game_tile_widget.dart
+++ lib/pages/games/widgets/games_game_tile_widget.dart
@@ -1,4 +1,5 @@
 import 'package:adwaita_icons/adwaita_icons.dart';
+import 'package:flash/l10n/l10n.dart';
 import 'package:flash/utils/game_repository/models/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -20,15 +21,15 @@ class GamesGameTile extends StatelessWidget {
         subtitle: Text(_game.processName),
         textColor: Colors.white,
         iconColor: Colors.white,
-        children: [_buildDeleteTile()],
+        children: [_buildDeleteTile(context)],
       )
     ]);
   }
 
-  Widget _buildDeleteTile() {
-    return const ListTile(
-      leading: AdwaitaIcon(AdwaitaIcons.edit_delete),
-      title: Text("Delete game"),
+  Widget _buildDeleteTile(BuildContext context) {
+    return ListTile(
+      leading: const AdwaitaIcon(AdwaitaIcons.edit_delete),
+      title: Text(context.l10n.deleteGame),
     );
   }
 }
