1a6984922090b4285daea36eab830372b46a5345
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-17 21:38:39 +0200

    Replace tile with ExpansionTile in GameTileWidget

--- lib/pages/games/widgets/games_game_tile_widget.dart
+++ lib/pages/games/widgets/games_game_tile_widget.dart
@@ -1,5 +1,6 @@
 import 'package:adwaita_icons/adwaita_icons.dart';
 import 'package:flash/utils/game_repository/models/game.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:libadwaita/libadwaita.dart';
 
@@ -13,11 +14,21 @@ class GamesGameTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AdwPreferencesGroup(children: [
-      AdwActionRow(
-        start: const AdwaitaIcon(AdwaitaIcons.gamepad),
-        title: _game.name,
-        subtitle: _game.processName,
+      ExpansionTile(
+        leading: const AdwaitaIcon(AdwaitaIcons.gamepad),
+        title: Text(_game.name),
+        subtitle: Text(_game.processName),
+        textColor: Colors.white,
+        iconColor: Colors.white,
+        children: [_buildDeleteTile()],
       )
     ]);
+  }
+
+  Widget _buildDeleteTile() {
+    return const ListTile(
+      leading: AdwaitaIcon(AdwaitaIcons.edit_delete),
+      title: Text("Delete game"),
+    );
   }
 }
