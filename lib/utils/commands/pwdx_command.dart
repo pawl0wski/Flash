40c96b326c939b17449ad6ced663c5da25c3b77b
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-17 22:49:24 +0200

    Attach scraper to PwdxCommand

--- lib/utils/commands/pwdx_command.dart
+++ lib/utils/commands/pwdx_command.dart
@@ -1,7 +1,24 @@
+import 'package:flash/utils/commands/scrapers/models/scraped_pwdx.dart';
+import 'package:flash/utils/commands/scrapers/pwdx_scraper.dart';
 import 'package:flash/utils/commands/validators/pwdx_validator.dart';
 
 import 'command.dart';
 
 class PwdxCommand extends Command {
-  PwdxCommand() : super("pwdx", validator: PwdxValidator());
+  PwdxCommand()
+      : super(
+          "pwdx",
+          validator: PwdxValidator(),
+          commandScraper: PwdxScraper(),
+        );
+
+  @override
+  ScrapedPwdx executeAndScrap(List<String> arguments) {
+    return super.executeAndScrap(arguments) as ScrapedPwdx;
+  }
+
+  @override
+  Future<ScrapedPwdx> executeAndScrapAsync(List<String> arguments) async {
+    return await super.executeAndScrapAsync(arguments) as ScrapedPwdx;
+  }
 }
