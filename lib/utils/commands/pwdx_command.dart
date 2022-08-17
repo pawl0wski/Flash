7ae42a560e62b47d5fcae1260a9940540f31b862
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-17 22:38:22 +0200

    Add validator to PwdxCommand

--- lib/utils/commands/pwdx_command.dart
+++ lib/utils/commands/pwdx_command.dart
@@ -1,5 +1,7 @@
+import 'package:flash/utils/commands/validators/pwdx_validator.dart';
+
 import 'command.dart';
 
 class PwdxCommand extends Command {
-  PwdxCommand() : super("pwdx");
+  PwdxCommand() : super("pwdx", validator: PwdxValidator());
 }
