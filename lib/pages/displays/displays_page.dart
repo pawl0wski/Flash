a4af79ed8f6d460b774f87c482f743c4ca651549
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-18 23:29:46 +0200

    Initialize bloc in DisplayPage

--- lib/pages/displays/displays_page.dart
+++ lib/pages/displays/displays_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/displays_bloc.dart';
 
 class DisplaysPage extends StatelessWidget {
   const DisplaysPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return _initializeBloc(child: Container());
+  }
+
+  Widget _initializeBloc({required Widget child}) {
+    return BlocProvider(
+      create: (BuildContext context) => DisplaysBloc(),
+      child: child,
+    );
   }
 }
