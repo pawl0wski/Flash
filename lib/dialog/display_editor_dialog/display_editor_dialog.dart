3450c4ce7e128455f24796a6a5264c779e84a0cb
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-20 23:14:38 +0200

    Build brightness slidebar in DisplayEditorDialog

--- lib/dialog/display_editor_dialog/display_editor_dialog.dart
+++ lib/dialog/display_editor_dialog/display_editor_dialog.dart
@@ -2,6 +2,7 @@ import 'package:flash/dialog/display_editor_dialog/bloc/display_editor_bloc.dart
 import 'package:flash/dialog/flash_dialog.dart';
 import 'package:flash/l10n/l10n.dart';
 import 'package:flash/utils/repository/models/display.dart';
+import 'package:flash/widgets/slider_with_label/slider_with_label.dart';
 import 'package:flash/widgets/text_field_with_label/text_field_with_label_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -34,7 +35,10 @@ class DisplayEditorDialog extends FlashDialog {
           width: 300,
           title: Text(context.l10n.editDisplay),
           padding: const EdgeInsets.only(bottom: 20, left: 15, right: 15),
-          children: [_buildTextField(context, state)]);
+          children: [
+            _buildTextField(context, state),
+            _buildBrightnessSlider(context, state)
+          ]);
     }
     return Container();
   }
@@ -48,5 +52,15 @@ class DisplayEditorDialog extends FlashDialog {
           .read<DisplayEditorBloc>()
           .add(DisplayEditorEventChangeName(newName: newName)),
     );
+  }
+
+  Widget _buildBrightnessSlider(
+      BuildContext context, DisplayEditorStateShowDisplay state) {
+    return SliderWithLabel(
+        label: context.l10n.brightness,
+        initialValue: state.display.brightness,
+        onChanged: (double newValue) => context
+            .read<DisplayEditorBloc>()
+            .add(DisplayEditorEventChangeBrightness(newBrightness: newValue)));
   }
 }
