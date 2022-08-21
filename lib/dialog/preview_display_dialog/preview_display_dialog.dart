b982f3e0e32acec5572322986802cbbe58bd5770
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-21 23:11:32 +0200

    Add initializeBloc in PreviewDisplayDialog

--- lib/dialog/preview_display_dialog/preview_display_dialog.dart
+++ lib/dialog/preview_display_dialog/preview_display_dialog.dart
@@ -1,4 +1,6 @@
+import 'package:flash/dialog/preview_display_dialog/bloc/preview_display_bloc.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:libadwaita/libadwaita.dart';
 
 import '../flash_dialog.dart';
@@ -6,6 +8,16 @@ import '../flash_dialog.dart';
 class PreviewDisplayDialog extends FlashDialog {
   @override
   Widget show(BuildContext context) {
-    return GtkDialog(children: []);
+    return _initializeBloc(
+        child: BlocConsumer<PreviewDisplayBloc, PreviewDisplayState>(
+            listener: (BuildContext context, PreviewDisplayState state) => {},
+            builder: (BuildContext context, PreviewDisplayState state) {
+              return const GtkDialog(children: []);
+            }));
+  }
+
+  _initializeBloc({required Widget child}) {
+    return BlocProvider<PreviewDisplayBloc>(
+        create: (BuildContext context) => PreviewDisplayBloc())
   }
 }
