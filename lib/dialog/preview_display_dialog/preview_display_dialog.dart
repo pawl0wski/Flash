247dc6fda270844a5ac5793f7347be66677014b8
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-22 19:33:23 +0200

    Show infoText in PreviewDisplayDialog

--- lib/dialog/preview_display_dialog/preview_display_dialog.dart
+++ lib/dialog/preview_display_dialog/preview_display_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flash/dialog/preview_display_dialog/bloc/preview_display_bloc.dart';
+import 'package:flash/l10n/l10n.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:libadwaita/libadwaita.dart';
@@ -12,12 +13,16 @@ class PreviewDisplayDialog extends FlashDialog {
         child: BlocConsumer<PreviewDisplayBloc, PreviewDisplayState>(
             listener: (BuildContext context, PreviewDisplayState state) => {},
             builder: (BuildContext context, PreviewDisplayState state) {
-              return const GtkDialog(children: []);
+              return GtkDialog(children: [_buildInfoText(context)]);
             }));
   }
 
   _initializeBloc({required Widget child}) {
     return BlocProvider<PreviewDisplayBloc>(
-        create: (BuildContext context) => PreviewDisplayBloc())
+        create: (BuildContext context) => PreviewDisplayBloc());
+  }
+
+  _buildInfoText(BuildContext context) {
+    return Text(context.l10n.acceptDisplay);
   }
 }
