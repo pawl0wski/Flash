e02a0d48e9163378e923cb21ebcbe8f2935e9152
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-24 00:53:16 +0200

    Add changeDisplay in XrandrCommand

--- lib/utils/commands/xrandr_command.dart
+++ lib/utils/commands/xrandr_command.dart
@@ -1,5 +1,6 @@
 import 'package:flash/utils/commands/command.dart';
 import 'package:flash/utils/process_adapter/process_adapter.dart';
+import 'package:flash/utils/repository/models/display.dart';
 
 class XRandrCommand extends Command {
   XRandrCommand({ProcessAdapter? processAdapter})
@@ -11,5 +12,22 @@ class XRandrCommand extends Command {
     return splitCommandOutputs.map((splitCommandOutput) {
       return splitCommandOutput.split(" ").last;
     }).toList();
+  }
+
+  void changeDisplay({required Display display, required String monitor}) {
+    var rgb = display.rgb;
+    List<String> displayRGBasList = [
+      rgb.r.toString(),
+      rgb.g.toString(),
+      rgb.b.toString()
+    ];
+    super.execute([
+      "-d",
+      monitor,
+      "--gamma",
+      ...displayRGBasList,
+      "--brightness",
+      display.brightness.toString()
+    ]);
   }
 }
