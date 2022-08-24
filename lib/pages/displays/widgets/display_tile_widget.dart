26026a696e1e8de46f0989427df054deab17b570
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-24 03:12:45 +0200

    Add edit and delete tile

--- lib/pages/displays/widgets/display_tile_widget.dart
+++ lib/pages/displays/widgets/display_tile_widget.dart
@@ -1,8 +1,10 @@
 import 'package:adwaita_icons/adwaita_icons.dart';
 import 'package:flash/l10n/l10n.dart';
+import 'package:flash/pages/displays/bloc/displays_bloc.dart';
 import 'package:flash/utils/repository/models/display.dart';
 import 'package:flash/widgets/secondary_tile/secondary_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:libadwaita/libadwaita.dart';
 
 class DisplayTile extends StatelessWidget {
@@ -18,7 +20,12 @@ class DisplayTile extends StatelessWidget {
       children: [
         ExpansionTile(
           title: Text(_display.name),
-          children: [_buildBrightnessTile(context), _buildGammaTile(context)],
+          children: [
+            _buildBrightnessTile(context),
+            _buildGammaTile(context),
+            _buildEditTile(context, display: _display),
+            _buildDeleteTile(context, display: _display)
+          ],
         )
       ],
     );
@@ -36,5 +43,25 @@ class DisplayTile extends StatelessWidget {
     return SecondaryTile(
         leading: const AdwaitaIcon(AdwaitaIcons.camera_flash),
         title: Text("${context.l10n.gamma} - $gamma"));
+  }
+
+  SecondaryTile _buildEditTile(BuildContext context,
+      {required Display display}) {
+    return SecondaryTile(
+      leading: const AdwaitaIcon(AdwaitaIcons.edit),
+      title: Text(context.l10n.editDisplay),
+      onTap: () =>
+          context.read<DisplaysBloc>().add(DisplaysEventEditDisplay(display)),
+    );
+  }
+
+  SecondaryTile _buildDeleteTile(BuildContext context,
+      {required Display display}) {
+    return SecondaryTile(
+      leading: const AdwaitaIcon(AdwaitaIcons.edit_delete),
+      title: Text(context.l10n.deleteDisplay),
+      onTap: () =>
+          context.read<DisplaysBloc>().add(DisplaysEventEditDisplay(display)),
+    );
   }
 }
