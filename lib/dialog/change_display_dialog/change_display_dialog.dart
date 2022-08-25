b1d5bcfd4505ac22170fc823599fedf76be2e953
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-25 22:51:37 +0200

    Add Buttons in ChangeDisplayDialog

--- lib/dialog/change_display_dialog/change_display_dialog.dart
+++ lib/dialog/change_display_dialog/change_display_dialog.dart
@@ -3,6 +3,8 @@ import 'package:flash/dialog/flash_dialog.dart';
 import 'package:flash/l10n/l10n.dart';
 import 'package:flash/utils/repository/models/display.dart';
 import 'package:flash/utils/repository/models/game.dart';
+import 'package:flash/widgets/expanded_button/expanded_button.dart';
+import 'package:flash/widgets/transparent_divider/transparent_divider_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:libadwaita/libadwaita.dart';
@@ -20,11 +22,16 @@ class ChangeDisplayDialog extends FlashDialog {
             builder: (context, state) {
               if (state is ChangeDisplayStateShowDisplays) {
                 return GtkDialog(
+                    title: Text(context.l10n.assignDisplay),
                     width: 450,
                     padding:
                         const EdgeInsets.only(bottom: 20, left: 15, right: 15),
                     children: [
                       _buildComboRow(context, state: state),
+                      const TransparentDivider(
+                        height: 15,
+                      ),
+                      _buildButtons(context),
                     ]);
               }
               return Container();
@@ -51,8 +58,24 @@ class ChangeDisplayDialog extends FlashDialog {
           },
           selectedIndex: state.currentIndex,
           choices: _changeDisplayListToStringList(state.displays),
-          title: context.l10n.assignDisplay,
+          title: context.l10n.display,
         )
+      ],
+    );
+  }
+
+  Widget _buildButtons(BuildContext context) {
+    return Row(
+      children: [
+        ExpandedButton(
+          onClick: () {},
+          mainButton: true,
+          text: context.l10n.save,
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        ExpandedButton(text: context.l10n.cancel, onClick: () => {})
       ],
     );
   }
