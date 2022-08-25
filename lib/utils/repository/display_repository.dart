32422a20c5f482b1ceaa392abe551a193ee76180
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-25 02:21:48 +0200

    Remove DisplayUuid from games when Display is deleted

--- lib/utils/repository/display_repository.dart
+++ lib/utils/repository/display_repository.dart
@@ -1,9 +1,29 @@
-import 'package:flash/utils/repository/hive_box/display_hive_box.dart';
-import 'package:flash/utils/repository/models/display.dart';
-
+import 'game_repository.dart';
+import 'hive_box/display_hive_box.dart';
+import 'models/display.dart';
+import 'models/game.dart';
 import 'repository.dart';
 
 class DisplayRepository extends Repository<Display> {
-  DisplayRepository({DisplayHiveBox? hiveBox})
-      : super(hiveBox: hiveBox ?? DisplayHiveBox());
+  final GameRepository _gameRepository;
+
+  DisplayRepository({DisplayHiveBox? hiveBox, GameRepository? gameRepository})
+      : _gameRepository = gameRepository ?? GameRepository(),
+        super(hiveBox: hiveBox ?? DisplayHiveBox());
+
+  @override
+  void delete(String uuid) {
+    _removeDisplayUuidFromAssociatedGames(uuid);
+    super.delete(uuid);
+  }
+
+  void _removeDisplayUuidFromAssociatedGames(String displayUuid) {
+    var allGames = _gameRepository.getAll();
+    var gamesWithDisplayUuid =
+        allGames.where((Game game) => game.displayUuid == displayUuid);
+    for (var game in gamesWithDisplayUuid) {
+      game.displayUuid = null;
+      _gameRepository.update(game);
+    }
+  }
 }
