7b929b0f61573c61a19e1b7b6e3673fc3bf24bfc
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-27 23:19:09 +0200

    Add TitleWidget to SettingsPage

--- lib/pages/settings/utils/page_builder/settings_page_builder.dart
+++ lib/pages/settings/utils/page_builder/settings_page_builder.dart
@@ -1,5 +1,8 @@
+import 'package:adwaita_icons/adwaita_icons.dart';
+import 'package:flash/l10n/l10n.dart';
 import 'package:flash/pages/settings/bloc/settings_bloc.dart';
 import 'package:flash/widgets/loading_widget/loading_widget.dart';
+import 'package:flash/widgets/title_widget/title_widget.dart';
 import 'package:flutter/widgets.dart';
 import 'package:libadwaita/libadwaita.dart';
 
@@ -10,7 +13,13 @@ class SettingsPageBuilder {
     if (state is SettingsStateShowSettings) {
       return AdwClamp.scrollable(
           child: Column(
-        children: [],
+        children: [
+          TitleWidget(
+              showBigAdwaitaIcon: false,
+              icon: AdwaitaIcons.settings,
+              title: context.l10n.settings,
+              description: context.l10n.settingsDescription)
+        ],
       ));
     }
     if (state is SettingsStateLoading) {
