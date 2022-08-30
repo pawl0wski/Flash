1910d04a06a7e5c18391283f07224d346df35c09
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-30 21:53:06 +0200

    Use SettingsManipulator in FlashActions

--- lib/utils/actions/flash_actions.dart
+++ lib/utils/actions/flash_actions.dart
@@ -1,9 +1,13 @@
+import 'package:flash/utils/settings/settings_manipulator.dart';
 import 'package:libadwaita_bitsdojo/libadwaita_bitsdojo.dart';
 
 extension LibAdwBitsdojo on AdwActions {
   AdwActions get flash {
+    var settingsManipulator = SettingsManipulator();
     return AdwActions(
-      onClose: appWindow?.hide,
+      onClose: settingsManipulator.closeWillHideWindow
+          ? appWindow?.hide
+          : appWindow?.close,
       onMaximize: appWindow?.maximizeOrRestore,
       onMinimize: appWindow?.minimize,
       onDoubleTap: appWindow?.maximizeOrRestore,
